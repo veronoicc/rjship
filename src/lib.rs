9d28f3e819f1d4356028757a5a72740780a6aec0
Andy <91694574+DunnAnDusted@users.noreply.github.com>
2023-11-26 13:46:37 +0000

    feat: Add RJSend::Error variant constructors (#11)
    As the RJSend::Success and RJSend::Fail variants are relatively simple,
    I decided constructors for these variants can be omitted from the library.
    
    By comparison, the RJSend::Error variant is significantly more complex,
    whilst still featuring a single required field.
    Consequently, constructors for this variant can be significantly more useful,
    providing shorthands when the optional fields aren't required,
    as well as options to map values to an instance of the type,
    when their representations align.
    
    * feat: Add basic Error variant constructor
    
    As `message` is the only required field for the `RJSend::Error` variant,
    this provides a basic constructor for this variant, which avoids
    having to specify `None` values, if not using the other fields.
    
    * feat: Add constructor from ErrorFields to Error
    
    As the `ErrorFields` struct is designed to map as directly as possible,
    to the `RJSend::Error` variant, it makes sense to allow it to behave
    *somewhat* like a builder for the variant.
    
    * feat: Add Error trait based constructors
    
    As the `RJSend::Error` variant maps pretty closely to the `Error` trait,
    in the core/std library, I felt adding constructors
    to handle this translation, might be useful.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use core::fmt;
+#[cfg(feature = "std")]
+use std::error::Error;
 
 use serde::Deserialize;
 
@@ -21,6 +23,73 @@ pub enum RJSend<D, FD, Msg = &'static str, ED = serde_json::Value> {
         code: Option<serde_json::Number>,
         data: Option<ED>,
     },
+}
+
+// Constructors functions
+impl<D, FD, Msg, ED> RJSend<D, FD, Msg, ED> {
+    #[inline]
+    pub const fn new_error(message: Msg) -> Self {
+        Self::Error {
+            message,
+            code: None,
+            data: None,
+        }
+    }
+
+    #[inline]
+    pub fn from_error_fields(
+        ErrorFields {
+            message,
+            code,
+            data,
+        }: ErrorFields<Msg, ED>,
+    ) -> Self {
+        Self::Error {
+            message,
+            code,
+            data,
+        }
+    }
+}
+
+// `std` dependant contructor functions
+#[cfg(feature = "std")]
+impl<D, FD, ED> RJSend<D, FD, String, ED> {
+    #[inline]
+    pub fn from_error(data: ED) -> Self
+    where
+        ED: Error,
+    {
+        let message = data.to_string();
+
+        Self::Error {
+            message,
+            code: None,
+            data: Some(data),
+        }
+    }
+}
+
+// Because `ErrorFields` is designed to map to `RJSend::Error` 
+// as directly as possible, it might be useful to have
+// an implementation which maps directly back...
+//
+// This also means `ErrorFields` can be used as an ad hoc builder
+// for the variant as well...
+impl<D, FD, Msg, ED> From<ErrorFields<Msg, ED>> for RJSend<D, FD, Msg, ED> {
+    fn from(fields: ErrorFields<Msg, ED>) -> Self {
+        Self::from_error_fields(fields)
+    }
+}
+
+#[cfg(feature = "std")]
+impl<D, FD, ED> From<ED> for RJSend<D, FD, String, ED>
+where
+    ED: Error,
+{
+    fn from(data: ED) -> Self {
+        Self::from_error(data)
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
